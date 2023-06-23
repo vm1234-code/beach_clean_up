68c045fcdfa8f06abd814b8094db71ed86ac1d4f
vm1234-code <vmachhi@DESKTOP-72MPA42>
2023-06-22 22:48:31 -0700

    Adding json file, the ability to exit out of text box for numeric text input, more styling

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, StyleSheet, ScrollView } from 'react-native';
+import { View, Text, TextInput, StyleSheet, ScrollView, TouchableWithoutFeedback, Keyboard, KeyboardAvoidingView } from 'react-native';
 
 const jsonData = require('./trashHeaders.json')
 
@@ -10,8 +10,19 @@ const App = () => {
     setInputValues((prevValues) => ({ ...prevValues, [title]: value }));
   };
 
+var date = new Date().getDate(); //To get the Current Date
+var month = new Date().getMonth() + 1; //To get the Current Month
+var year = new Date().getFullYear(); //To get the Current Year
+
+
   return (
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+    <KeyboardAvoidingView behavior="position" style={styles.container}>
+
+    <Text style={styles.mainTitle}>Surfrider Beach Clean-Up {'\n'} {month}/{date}/{year}</Text>
+
     <View style={styles.container}>
+    <ScrollView style={styles.scrollContainer}>
       {Object.entries(jsonData).map(([title, values]) => (
         <View key={title} style={styles.rowContainer}>
           <Text style={styles.title}>{title}</Text>
@@ -30,24 +41,42 @@ const App = () => {
 
         </View>
       ))}
+    </ScrollView>
     </View>
+    
+    </KeyboardAvoidingView>
+    </TouchableWithoutFeedback>
+    
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  mainTitle:{
+    textAlign: 'center',
+    paddingTop: 60,
+  },
+  scrollContainer:{
     flex: 1,
+  },
+  container: {
+    flexGrow: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    padding: 16,
+    paddingTop: 50,
+
   },
   rowContainer: {
     marginBottom: 16,
+    backgroundColor: 'skyblue',
+    justifyContent:'center',
+    alignItems: 'center',
+    width: 250,
   },
   title: {
     fontSize: 18,
     fontWeight: 'bold',
     marginBottom: 8,
+    alignSelf: 'flex-start'
   },
   input: {
     borderWidth: 1,
