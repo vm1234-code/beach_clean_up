8d645be96241794af190922efc93a608b1a1404e
vm1234-code <virafmachhi@gmail.com>
2023-11-13 08:12:17 -0800

    Adding comments

--- App.js
+++ App.js
@@ -1,34 +1,51 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, StyleSheet, ScrollView, TouchableWithoutFeedback, Keyboard, KeyboardAvoidingView } from 'react-native';
 
+// Load JSON data from the file 'trashHeaders'
 const jsonData = require('./trashHeaders.json')
 
+//Functional component named 'App'
 const App = () => {
-  const [inputValues, setInputValues] = useState({});
 
+  // State to manage input values
+  const [inputValues, setInputValues] = useState({});
+  
+  //Function to handle changes in TextInput
   const handleInputChange = (title, value) => {
     setInputValues((prevValues) => ({ ...prevValues, [title]: value }));
   };
 
+// Get current date, month, and year
 var date = new Date().getDate(); //To get the Current Date
 var month = new Date().getMonth() + 1; //To get the Current Month
 var year = new Date().getFullYear(); //To get the Current Year
 
-//if 
-
-
+// Render the UI
   return (
+
+    // Dismiss the keyboard when tapping outside of a TextInput
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+
+    {/* Adjust the view when the keyboard is open */}
     <KeyboardAvoidingView behavior="position" style={styles.container}>
     <Text style={styles.mainTitle}>Surfrider Beach Clean-Up {'\n'} {month}/{date}/{year}</Text>
-   
+
+    {/* Scrollable container for the form */}
     <ScrollView style={styles.scrollContainer}>
+
+      {/* Map through the JSON data and render input fields */}
       {Object.entries(jsonData).map(([title, values]) => (
         <View key={title} style={styles.rowContainer}>
+          
+          {/* Display the title for the current section */}
           <Text style={styles.title}>{title}</Text>
+
+          {/* Map through values in the current section and render input fields and title */}
           {values.map((value, index) => (
             <View style={styles.inputLine} key={index}>
               <Text style={styles.inputTitle}>{value}</Text>
+              
+              {/** Input field for entering numeric values */}
               <TextInput
                 style={styles.inputNumber}
                 placeholder="Enter value"
@@ -45,11 +62,7 @@ var year = new Date().getFullYear(); //To get the Current Year
         
       ))}
 
-      <TextInput
-                style={styles.input}
-                placeholder="Enter value"
-                keyboardType="numeric"
-      />
+      
 
     
     </ScrollView>
@@ -59,6 +72,7 @@ var year = new Date().getFullYear(); //To get the Current Year
   );
 };
 
+// Styles for the components
 const styles = StyleSheet.create({
   mainTitle:{
     textAlign: 'center',
